54769c4695bbcf5645e4e5981672795bfd329460
Max <133934232+Kleostro@users.noreply.github.com>
2024-09-20 21:08:24 +0300

    refactor: fs create

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,50 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const filesFolderName = 'files';
+const filesCopyFolderName = 'files_copy';
+const filesFolderPath = join(__dirname, filesFolderName);
+
+const findFilesFolder = async (folderPath) => {
+    try {
+        const filesFolder = await promises.readdir(folderPath);
+        return filesFolder;
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.error('FS operation failed');
+        } else {
+            console.error(err);
+        }
+        return null;
+    }
+};
+
+const createCopyFilesFolder = async (dirname, folderCopyName) => {
+    try {
+        await promises.mkdir(join(dirname, folderCopyName));
+        return true;
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            console.error('FS operation failed');
+        } else {
+            console.error(err);
+        }
+        return false;
+    }
+};
+
 const copy = async () => {
-    // Write your code here 
+    const filesFolder = await findFilesFolder(filesFolderPath);
+    if (!filesFolder) {
+        return;
+    }
+
+    const copyFilesFolder = await createCopyFilesFolder(__dirname, filesCopyFolderName);
+    if (!copyFilesFolder) {
+        return;
+    }
+    await Promise.all(filesFolder.map(async (file) => await promises.copyFile(join(filesFolderPath, file), join(__dirname, filesCopyFolderName, file))));
 };
 
 await copy();
