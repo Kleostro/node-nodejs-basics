434c297a932f0400d44a6bbe45198532708ba77d
Max <133934232+Kleostro@users.noreply.github.com>
2024-09-20 22:58:56 +0300

    feat: implement fs list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,35 @@
+import { promises } from 'fs';
+import { join, basename } from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const filesFolderName = 'files';
+const filesFolderPath = join(__dirname, filesFolderName);
+
+const readFilesFolder = async (folderPath) => {
+    try {
+        const filesFolder = await promises.readdir(folderPath);
+        return filesFolder;
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.error('FS operation failed');
+        } else {
+            console.error(err);
+        }
+        return null;
+    }
+};
+
 const list = async () => {
-    // Write your code here 
+    const filesFolder = await readFilesFolder(filesFolderPath);
+    if (!filesFolder) {
+        return;
+    }
+    const fileNamesArr = filesFolder.map((file) => basename(file));
+    const fileNamesList = fileNamesArr.join('\n');
+
+    console.table(fileNamesArr);
+    console.log('\n');
+    console.log(fileNamesList);
 };
 
 await list();
