9308795fe59c0500b96bb6e005a199b99cfb4d30
Max <133934232+Kleostro@users.noreply.github.com>
2024-09-20 21:53:34 +0300

    feat: implement fs delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+
+import { promises } from 'fs';
+import { join } from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const filesFolderName = 'files';
+const deletedFileName = 'fileToRemove.txt';
+const deletedFile = join(__dirname, filesFolderName, deletedFileName);
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await promises.access(deletedFile);
+        await promises.unlink(deletedFile);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.error('FS operation failed');
+        } else {
+            console.error(err);
+        }
+    }
 };
 
 await remove();
