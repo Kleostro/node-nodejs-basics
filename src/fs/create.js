9a6023fbd95f787bcb0325ef56fe08e694c5aa67
Max <133934232+Kleostro@users.noreply.github.com>
2024-09-20 20:32:14 +0300

    feat: implement fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { promises } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fresh.txt');
+
+    try {
+        await promises.access(filePath);
+        console.error('FS operation failed');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            await promises.writeFile(filePath, 'I am fresh and young');
+        } else {
+            console.error(err);
+        }
+    }
 };
 
 await create();
