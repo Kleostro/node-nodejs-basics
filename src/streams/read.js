d56e1ae7a806a2516bfb2892a67ebef743428a4b
Max <133934232+Kleostro@users.noreply.github.com>
2024-09-21 16:50:05 +0300

    feat: implement stream to read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { stdout } from 'process';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const filesFolderName = 'files';
+const readFileName = 'fileToRead.txt';
+const readFile = join(__dirname, filesFolderName, readFileName);
+
 const read = async () => {
-    // Write your code here 
+    const readStream = createReadStream(readFile, 'utf-8');
+    readStream.on("data", (chunck) => stdout.write(chunck + "\n"))
 };
 
 await read();
