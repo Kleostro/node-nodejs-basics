594e521df280b81bc16eb9dc3a5412dfdd48a2a5
Max <133934232+Kleostro@users.noreply.github.com>
2024-09-22 01:08:14 +0300

    feat: implement zip decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,35 @@
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream, promises } from 'fs';
+import { join } from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const filesFolderName = 'files';
+const fileToCompressName = 'fileToCompress.txt';
+const compressedFileName = 'archive.gz';
+const fileToCompress = join(__dirname, filesFolderName, fileToCompressName);
+const compressedFile = join(__dirname, filesFolderName, compressedFileName);
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        await promises.access(compressedFile);
+
+        const gunzip = createGunzip();
+        const readStream = createReadStream(compressedFile);
+        const writeStream = createWriteStream(fileToCompress);
+
+        readStream.pipe(gunzip).pipe(writeStream);
+
+        writeStream.on('finish', async () => {
+            console.log('\x1b[32mDecompression successful!');
+            await promises.unlink(compressedFile);
+        });
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.error('Decompression failed');
+        } else {
+            console.error(err);
+        }
+    }
 };
 
 await decompress();
