fdf95de3478be00ffcf1eae7a539ed12a2c6ab68
Max <133934232+Kleostro@users.noreply.github.com>
2024-09-26 18:03:10 +0300

    refactor: config stdio in cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,16 +6,23 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const filesFolderName = 'files';
 const scriptFileName = 'script.js';
 const scriptFileNamePath = join(__dirname, filesFolderName, scriptFileName);
-
 const spawnChildProcess = async (args) => {
     const childProcess = spawn('node', [scriptFileNamePath, ...args], {
-        stdio: ['pipe', 'pipe', 'pipe'],
+        stdio: ['pipe', 'pipe', 'inherit'],
     });
 
     process.stdin.pipe(childProcess.stdin);
     childProcess.stdout.pipe(process.stdout);
-    return childProcess;
+
+    return new Promise((resolve, reject) => {
+        childProcess.on('exit', (code) => {
+            if (code === 0) {
+                resolve();
+            } else {
+                reject(new Error(`Child process exited with code ${code}`));
+            }
+        });
+    });
 };
 
-spawnChildProcess(['someArgument1', 'someArgument2']);
-
+spawnChildProcess(['someArgument1', 'someArgument2']).catch(console.error);
